28f78a66990e936dd7a09083af16ea5021c03fee
paullobo <paullobo500@gmail.com>
2020-07-02 05:15:20 +0530

    Initial Commit

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,6 @@ const auth = require("../utils/authMiddleware")
 router.put('/addAgency',auth.verifySubscription,authController.addUpdateAgency);
 router.post('/updateClient',auth.verifySubscription,authController.updateClient);
 router.delete('/deleteAgency',auth.verifySubscription,authController.deleteAgency);
-router.get('/agencies',auth.verifySubscription,authController.getClients);
+router.get('/clients',auth.verifySubscription,authController.getClients);
 
 module.exports = router
